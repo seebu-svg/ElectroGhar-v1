5a6a02710b1947c79780e87f88483cc4dbc184ab
seebu-svg <haseeb.rehman.khalid@gmail.com>
2026-09-05 15:53:40 +0500

    api setting for product

--- api/index.mjs
+++ api/index.mjs
@@ -1,3 +1,11 @@
-import { app } from "../server/src/app.js";
-
-export default app;
+export default async function handler(req, res) {
+  try {
+    const { app } = await import("../server/src/app.js");
+    return app(req, res);
+  } catch (err) {
+    console.error("Failed to start API:", err);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: err.message || "Internal server error" }));
+  }
+}
